7b5c5a4f7f61939ca2a0ddf99a60ea99c8ea575a
Luzin7 <lvictor.dutra@gmail.com>
2024-08-24 23:45:01 -0300

    initial commit

---
+++ basic-calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿int n1, n2, result = 0;
+string operation;
+
+Console.Write("Enter first number: ");
+n1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+n2 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter an operator (+, -, *, /): ");
+operation = Console.ReadLine();
+
+switch (operation)
+{ 
+    case "+":
+        result = n1 + n2;
+        break;
+    case "-":
+        result = n1 - n2;
+        break;
+    case "*":
+        result = n1 * n2;
+        break;
+    case "/":
+        if (n2 == 0)
+        {
+            Console.WriteLine("Error: Division by zero is not allowed.");
+            return;
+        }
+        result = n1 / n2;
+        break;
+    default:
+        Console.WriteLine("Error: Invalid operator. Please use one of the following: +, -, *, /");
+        return;
+}
+
+Console.WriteLine(result);
